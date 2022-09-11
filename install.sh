4e34a061b2b93d0cd26aa4f99fe4ab118e0b831b
Musa Ibrahim <=>
2022-09-11 20:34:46 +0100

    Auto Commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "unpacking binaries..."
+
+GITA="gita.sh"
+BIN_PATH="${HOME}/../usr/bin"
+WRAPPER="gita"
+
+cp "${GITA}" "${BIN_PATH}/${GITA}"
+chmod 711 "${BIN_PATH}/${GITA}"
+ln -s "${BIN_PATH}/${GITA}" "${BIN_PATH}/${WRAPPER}"
