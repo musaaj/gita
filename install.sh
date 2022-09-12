39d55beca287a806b0e1f9c43f642c45e3409c21
Musa Ibrahim <=>
2022-09-12 11:11:01 +0100

    Auto Commit

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ GITA="gita.sh"
 BIN_PATH="${HOME}/../usr/bin"
 WRAPPER="gita"
 
+if [ "${BIN_PATH}/${WRAPPER}" ]; then
+	echo "gita already installed"
+	exit 1
+fi
+
 cp "${GITA}" "${BIN_PATH}/${GITA}"
 chmod 711 "${BIN_PATH}/${GITA}"
 ln -s "${BIN_PATH}/${GITA}" "${BIN_PATH}/${WRAPPER}"
